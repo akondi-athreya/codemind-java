5b1188f4e428ad5b71d42f3a8bb36b42ef1ca960
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-04 21:46:32 +0530

    import java.util.Scanner;
    public class code{
        public static void main (String[] args) {
            Scanner my = new Scanner(System.in);
            float n;
            n = my.nextFloat();
            System.out.printf("%.2f",3.14*n*n);
            System.out.printf("
    %.2f",2*3.14*n);
        }
    }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class code{
+    public static void main (String[] args) {
+        Scanner my = new Scanner(System.in);
+        float n;
+        n = my.nextFloat();
+        System.out.printf("%.2f",3.14*n*n);
+        System.out.printf("
+%.2f",2*3.14*n);
+    }
+}
