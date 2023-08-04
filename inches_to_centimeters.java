7c8983ca2b99d5a0bd8e50954a95f20b8dda2f4f
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-04 21:25:44 +0530

    import java.util.Scanner;
    public class sample{
        public static void main(String args[]){
            Scanner my = new Scanner(System.in);
            float n1;
            n1 = my.nextFloat();
            System.out.printf("%.2f",n1*2.54);
        }
    }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class sample{
+    public static void main(String args[]){
+        Scanner my = new Scanner(System.in);
+        float n1;
+        n1 = my.nextFloat();
+        System.out.printf("%.2f",n1*2.54);
+    }
+}
