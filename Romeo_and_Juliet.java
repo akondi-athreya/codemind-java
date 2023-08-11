d84c5dcdd4ac8f1a70ac28ae655aa4cf8462b6f1
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-11 21:29:14 +0530

    import java.util.*;
    public class code{
        public static void main (String[] args) {
            Scanner my = new Scanner(System.in);
            int a,b,c,d;
            a = my.nextInt();
            b = my.nextInt();
            c = my.nextInt();
            d = (a*5)+(b*10);
            System.out.println(d/c);
        }
    }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class code{
+    public static void main (String[] args) {
+        Scanner my = new Scanner(System.in);
+        int a,b,c,d;
+        a = my.nextInt();
+        b = my.nextInt();
+        c = my.nextInt();
+        d = (a*5)+(b*10);
+        System.out.println(d/c);
+    }
+}
