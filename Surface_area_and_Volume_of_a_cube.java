6e4ac0b144c3a1f6de46294d8c69a4be71a6e228
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-11 22:04:48 +0530

    import java.util.*;
    public class codemind {
        public static void main(String[] args) {
            Scanner my = new Scanner(System.in);
            int n;
            n = my.nextInt();
            System.out.printf("Surface area = %d and Volume = %d",6*n*n,n*n*n);
        }
    }

---
+++ Surface_area_and_Volume_of_a_cube.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        int n;
+        n = my.nextInt();
+        System.out.printf("Surface area = %d and Volume = %d",6*n*n,n*n*n);
+    }
+}
