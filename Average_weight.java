40a43061c36e76afa7cded272ee6fe83031753a4
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-16 21:47:33 +0530

    import java.util.*;
    public class code{
        public static void main (String[] args) {
            Scanner my = new Scanner(System.in);
            int x,a,b,c;
            x = my.nextInt();
            a = my.nextInt();
            b = my.nextInt();
            c = (3*x)-a-b;
            System.out.println(c);
        }
    }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class code{
+    public static void main (String[] args) {
+        Scanner my = new Scanner(System.in);
+        int x,a,b,c;
+        x = my.nextInt();
+        a = my.nextInt();
+        b = my.nextInt();
+        c = (3*x)-a-b;
+        System.out.println(c);
+    }
+}
