f743943d00f39fde15865476a589df61e2974d49
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 21:32:27 +0530

    import java.util.*; public class codemind {     public static void main(String[] args) {         Scanner my = new Scanner(System.in);         double a,b;         a = my.nextDouble();         b = my.nextDouble();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        double a,b;
+        a = my.nextDouble();
+        b = my.nextDouble();
+        System.out.printf("%.2f",a*b);
+    }
+}
