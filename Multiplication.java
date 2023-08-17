ce81bb49579c6700429a22916d48a5e6e718f659
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 21:29:00 +0530

    import java.util.*; public class codemind {     public static void main(String[] args) {         Scanner my = new Scanner(System.in);         int a,b;         a = my.nextInt();         b = my.nextInt();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        int a,b;
+        a = my.nextInt();
+        b = my.nextInt();
+        System.out.println(a*b);
+    }
+}
