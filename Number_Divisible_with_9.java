5ad46c052a140fdb2247b99023e046211225b270
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 10:53:42 +0530

    import java.util.*; public class code{     public static void main(String[] args){         Scanner my = new Scanner(System.in);         int a = my.nextInt();         if (a%9==0) System.out.println("True");         else System.out.print("False");     } }

---
+++ Number_Divisible_with_9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class code{
+    public static void main(String[] args){
+        Scanner my = new Scanner(System.in);
+        int a = my.nextInt();
+        if (a%9==0) System.out.println("True");
+        else System.out.print("False");
+    }
+}
