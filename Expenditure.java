a8673a3d0fc49d963199b0b19fba101d7c85600e
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 11:03:16 +0530

    import java.util.*;
    public class code{
        public static void main (String[] args) {
            Scanner my = new Scanner(System.in);
            int a = my.nextInt();
            int b = my.nextInt();
            if (30*b <= a) System.out.println("YES");
            else System.out.println("NO");
        }
    }

---
+++ Expenditure.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class code{
+    public static void main (String[] args) {
+        Scanner my = new Scanner(System.in);
+        int a = my.nextInt();
+        int b = my.nextInt();
+        if (30*b <= a) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
