dd66a0df99e91c0cc511ead77315d3933e0ba0ce
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 21:20:02 +0530

    import java.util.*; public class code{     public static void main(String[] args){         Scanner my = new Scanner(System.in);         String ch = my.nextLine();         System.out.println(ch);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class code{
+    public static void main(String[] args){
+        Scanner my = new Scanner(System.in);
+        String ch = my.nextLine();
+        System.out.println(ch);
+    }
+}
