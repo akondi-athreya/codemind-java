1fc25551f791f4eb587111a09a574ad6f5b6ee8d
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 21:07:19 +0530

    import java.util.*; public class codemind {     public static void main(String[] args) {         Scanner my = new Scanner(System.in);         int a,b,c,d;         a = my.nextInt();         b = my.nextInt();         c = my.nextInt();         d = my.nextInt();         if(a>=10 && b>=10 && c>=10 && d>=10) System.out.println("YES");         else System.out.println("NO");     } }

---
+++ Target.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        int a,b,c,d;
+        a = my.nextInt();
+        b = my.nextInt();
+        c = my.nextInt();
+        d = my.nextInt();
+        if(a>=10 && b>=10 && c>=10 && d>=10) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
