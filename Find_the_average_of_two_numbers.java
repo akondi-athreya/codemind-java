741b209662293422fdaaff186b5a534a2a919edf
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-17 21:34:59 +0530

    import java.util.*; public class codemind {     public static void main(String[] args) {         Scanner my = new Scanner(System.in);         Double a,b;         a = my.nextDouble();         b = my.nextDouble();         System.out.printf("%.4f",(a+b)/2);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        Double a,b;
+        a = my.nextDouble();
+        b = my.nextDouble();
+        System.out.printf("%.4f",(a+b)/2);
+    }
+}
