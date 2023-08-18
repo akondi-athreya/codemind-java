8b7107a01eed80036aafc6c8b453cfec8d4d40c3
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-18 11:40:31 +0530

    import java.util.*; public class codemind {     public static void main(String[] args) {         Scanner my = new Scanner(System.in);         int m,n,i,c=0;         m = my.nextInt();         n = my.nextInt();         for(i=m ; i<=n ; i++){             if(i%3==0) c+=1;         }         System.out.print(c);     } }

---
+++ Count_Multiples_of_3_between_M_,N.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        int m,n,i,c=0;
+        m = my.nextInt();
+        n = my.nextInt();
+        for(i=m ; i<=n ; i++){
+            if(i%3==0) c+=1;
+        }
+        System.out.print(c);
+    }
+}
