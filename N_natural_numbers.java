8622a9419ef9072bbe1b76e6456f467be6feafae
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-18 11:26:56 +0530

    import java.util.*; public class codemind {     public static void main(String[] args) {         Scanner my = new Scanner(System.in);         int n = my.nextInt();         for(int i=1 ; i<=n ; i++){             System.out.print(i + " ");         }     } }

---
+++ N_natural_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] args) {
+        Scanner my = new Scanner(System.in);
+        int n = my.nextInt();
+        for(int i=1 ; i<=n ; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
