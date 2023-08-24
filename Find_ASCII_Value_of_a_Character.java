4e18eb5e75774d5202d7f15b0d2a2860561b55cd
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-24 22:38:28 +0530

    import java.util.*; public class code{     public static void main(String[] args){         Scanner my = new Scanner(System.in);         String i = my.next();         System.out.println((int)i.charAt(0));     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class code{
+    public static void main(String[] args){
+        Scanner my = new Scanner(System.in);
+        String i = my.next();
+        System.out.println((int)i.charAt(0));
+    }
+}
