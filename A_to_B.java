e83dcc21f4bfdf474a4f48cfd26ec68ded4eac4f
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-25 11:14:46 +0530

    import java.util.*; public class code{     public static void main(String[] king){         Scanner my = new Scanner(System.in);         int a = my.nextInt();         int b = my.nextInt();         for(int i=a ; i<=b ; i++){             System.out.print(i+" ");         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class code{
+    public static void main(String[] king){
+        Scanner my = new Scanner(System.in);
+        int a = my.nextInt();
+        int b = my.nextInt();
+        for(int i=a ; i<=b ; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
