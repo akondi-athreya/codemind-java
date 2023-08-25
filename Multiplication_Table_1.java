711217925866aeab48d20768078d8bab63f1f5b5
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-25 11:11:41 +0530

    import java.util.*; public class code{     public static void main(String[] ironman){         Scanner my = new Scanner(System.in);         int n = my.nextInt();         for(int i=1 ; i<=12 ; i++){             System.out.println(n+" x "+i+" = "+n*i);         }     } }

---
+++ Multiplication_Table_1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class code{
+    public static void main(String[] ironman){
+        Scanner my = new Scanner(System.in);
+        int n = my.nextInt();
+        for(int i=1 ; i<=12 ; i++){
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+}
