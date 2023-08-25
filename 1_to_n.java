f5ae2a459df146965985f96c4ef2842aed1ec648
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-25 11:17:08 +0530

    import java.util.*; public class code{     public static void main(String[] queen){         Scanner my = new Scanner(System.in);         int a = my.nextInt();         for(int i=1 ; i<=a ; i++){             System.out.print(i+" ");         }     } }

---
+++ 1_to_n.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class code{
+    public static void main(String[] queen){
+        Scanner my = new Scanner(System.in);
+        int a = my.nextInt();
+        for(int i=1 ; i<=a ; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
