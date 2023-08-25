87ff0aa52d521c9b7bd2fa8d22712f8e1b98e3cc
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-25 09:35:10 +0530

    import java.util.*; public class code{     public static void main(String[] avengers){         Scanner my = new Scanner(System.in);         int n = my.nextInt();         double i = 3.14*n*n;         System.out.printf("%.2f",i);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class code{
+    public static void main(String[] avengers){
+        Scanner my = new Scanner(System.in);
+        int n = my.nextInt();
+        double i = 3.14*n*n;
+        System.out.printf("%.2f",i);
+    }
+}
