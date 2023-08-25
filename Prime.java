a51d2d8b3b5b696d0382b1f9b4408b866eee9c22
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-08-25 21:51:38 +0530

    import java.util.*; public class shinchan{     public static void main (String[] saske) {         Scanner my = new Scanner(System.in);         int n = my.nextInt();         int c = 0;         for(int i=2;i<n;i++){             if(n%i==0){                 System.out.println("Not Prime");                 c+=1;                 break;             }         }         if(c==0) System.out.println("Prime");     } }

--- Prime.java
+++ Prime.java
@@ -1,19 +1,16 @@
 import java.util.*;
-public class codemind {
-    public static void main(String[] args) {
+public class shinchan{
+    public static void main (String[] saske) {
         Scanner my = new Scanner(System.in);
         int n = my.nextInt();
-        int c=0;
-        for(int i=2 ; i<n ; i++){
-            if(n%i==0) {
-                c=0;
-                System.out.print("Not Prime");
+        int c = 0;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                System.out.println("Not Prime");
+                c+=1;
                 break;
             }
-            else{
-                c+=1;
-            }
         }
-        if(c>0) System.out.print("Prime");
+        if(c==0) System.out.println("Prime");
     }
 }
