4d5f893fa82f9df5cdfc8234a59534e442b411de
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-09-08 21:26:26 +0530

    import java.util.*; public class codemind{     public static void main(String[] args){         Scanner my = new Scanner(System.in);         String a = my.next();         StringBuilder rev = new StringBuilder(a).reverse();         String b = rev.toString();         if(a.equals(b)){             System.out.println("True");         }         else{             System.out.println("False");         }     } }

--- Palindrome.java
+++ Palindrome.java
@@ -1,16 +1,15 @@
 import java.util.*;
-public class codemind {
-    public static void main(String[] args) {
+public class codemind{
+    public static void main(String[] args){
         Scanner my = new Scanner(System.in);
-        int n = my.nextInt();
-        int rem=0,rev=0;
-        int temp=n;
-        while(n!=0){
-            rem = n%10;
-            rev = rev*10+rem;
-            n/=10;
+        String a = my.next();
+        StringBuilder rev = new StringBuilder(a).reverse();
+        String b = rev.toString();
+        if(a.equals(b)){
+            System.out.println("True");
         }
-        if(rev==temp) System.out.println("Palindrome");
-        else System.out.println("Not Palindrome");
+        else{
+            System.out.println("False");
+        }
     }
 }
