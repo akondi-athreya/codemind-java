83d8a66874a2ff664e8efc33cb0620383b4f64c5
akondi-athreya <119722868+akondi-athreya@users.noreply.github.com>
2023-10-23 23:36:57 +0530

    import java.util.*; public class code{     public static void main(String[] args){         Scanner my = new Scanner(System.in);         float C = my.nextFloat();         float F = 32+(C * 9/5);         System.out.printf("%.2f",F);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class code{
+    public static void main(String[] args){
+        Scanner my = new Scanner(System.in);
+        float C = my.nextFloat();
+        float F = 32+(C * 9/5);
+        System.out.printf("%.2f",F);
+        
+    }
+}
